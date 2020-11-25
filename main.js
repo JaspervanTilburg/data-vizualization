9141d5fc7ad9ac7fac4775afe448ffd728be0dc1
JaspervanTilburg <jaspert95@gmail.com>
2020-11-25 15:39:19 +0100

    Plot HMPalen

--- main.js
+++ main.js
@@ -9,22 +9,42 @@ var projection = d3.geoMercator()
 var path = d3.geoPath()
     .projection(projection);
 
-// Join the FeatureCollection's features array to path elements
 var svg = d3.select('svg')
   .attr("width", width)
   .attr("height", height);
+var map = svg.append("g");
 
 function drawMap(json) {
   var nl = topojson.feature(json, json.objects.gemeente_2020);
-  var u = svg.selectAll('path')
-    .data(nl.features);
-  
-  // Create path elements and update the d attribute using the geo generator
-  u.enter()
-    .append('path')
-    .attr('d', path);
+  map.selectAll('path')
+    .data(nl.features)
+    .enter()
+      .append('path')
+      .attr('d', path);
+}
+
+function drawHMpaal(json) {
+  // map.selectAll('path')
+  //   .data(json.features)
+  //   .enter()
+  //     .append('path')
+  //     .attr('d', path)
+  //     .attr('fill', 'red');
+
+  map.selectAll('dot')
+    .data(json.features)
+    .enter()
+      .append('circle')
+      .attr("class", "dot")
+      .attr("r", 1)
+      .attr("cx", function(d){return projection(d.geometry.coordinates)[0];})
+      .attr("cy", function(d){return projection(d.geometry.coordinates)[1];});
 }
 
 d3.json('nl_grenzen_topo.json').then(function(json) {
-    drawMap(json);
-  })
+  drawMap(json);
+});
+
+d3.json('hmpaal_data.json').then(function(json) {
+  drawHMpaal(json)
+});
