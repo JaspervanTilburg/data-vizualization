1b47bc7234d6170c9ae45542ff25dccddf0b556f
JaspervanTilburg <jaspert95@gmail.com>
2020-12-02 22:35:32 +0100

    Add day selection

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ var width		= 750,
     centerLat		= 5.5,	
     centerLong  	= 52.2;	 
 
+var geoData;
+var hmData;
+var fileData;
+
 var projection = d3.geoMercator()
     .scale(scale).translate([width / 2, height / 2]).center([centerLat, centerLong]);
 var path = d3.geoPath()
@@ -14,17 +18,19 @@ var svg = d3.select('svg')
   .attr("height", height);
 var map = svg.append("g");
 
-function drawMap(json) {
-  var nl = topojson.feature(json, json.objects.gemeente_2020);
-  map.selectAll('path')
+var file;
+
+function drawMap() {
+  var nl = topojson.feature(geoData, geoData.objects.gemeente_2020);
+  map.selectAll('paadjes')
     .data(nl.features)
     .enter()
       .append('path')
       .attr('d', path);
 }
 
-function drawHMpaal(json) {
-  var filtered = json.features.filter(function(d) { return Number.isInteger(d.properties.Hectometerpole); });
+function drawHMpaal() {
+  var filtered = hmData.features.filter(function(d) { return Number.isInteger(d.properties.Hectometerpole); });
   
   // map.selectAll('path')
   //   .data(filtered)
@@ -43,10 +49,49 @@ function drawHMpaal(json) {
       .attr("cy", function(d){return projection(d.geometry.coordinates)[1];});
 }
 
+function drawFile(day) {
+  var filtered = fileData.filter(function(d) {return d.DatumFileBegin === "10/" + day + "/2020";})
+  file = map.selectAll("data")
+    .data(filtered)
+    .enter().append('g')
+      .selectAll('file')
+      .data(function(d) {return JSON.parse(d.coordinates);})
+      .enter().append('circle')
+        .attr("r", 2)
+        .attr("cx", function(e) {return projection(e)[0];})
+        .attr("cy", function(e) {return projection(e)[1];})
+        // .style("fill", "red");
+  }
+
+function isFile(d, fileData) {
+  for (i = 0; i < fileData.length; i++) {
+    var file = fileData[i];
+    var fileStart = Math.min(parseFloat(file.HectometerKop.replace(",", ".")), parseFloat(file.HectometerStaart.replace(",", ".")));
+    var fileEnd = Math.max(parseFloat(file.HectometerKop.replace(",", ".")), parseFloat(file.HectometerStaart.replace(",", ".")));
+    if (d.properties.Highway == file.RouteOms && fileStart <= d.properties.Hectometerpole && d.properties.Hectometerpole <= fileEnd) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function select_day() {
+  file.remove();
+  var day = d3.select("#day").node().value;
+  drawFile(day);
+}
+
 d3.json('nl_grenzen_topo.json').then(function(json) {
-  drawMap(json);
+  geoData = json;
+  drawMap();
 });
 
 d3.json('hmpaal_data.json').then(function(json) {
-  drawHMpaal(json)
+  hmData = json;
+  drawHMpaal()
+  drawFile(1)
 });
+
+d3.csv('files_2020_10_with_coordinates.csv').then(function(csv) {
+  fileData = csv;
+})
